80b790ac3df415b4268ae241ef4ad2a0d9da903f
Devina Mendez <devmendez@gmail.com>
2023-10-23 21:05:45 -0500

    first commit

---
+++ traversy-js-challenges/01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -0,0 +1,19 @@
+// function countOccurrences(str, char) {
+//   let count = 0;
+
+//   for(let i = 0; i <str.length; i++) {
+//     if (str[i] === char) {
+//       count++;
+//     }
+//   }
+
+//   return count;
+// }
+
+// const countOccurrences = (str, char) => {
+//   return str.split(char).length - 1; 
+// }
+
+const countOccurrences = (str, char) => str.split(char).length - 1; 
+
+module.exports = countOccurrences;
